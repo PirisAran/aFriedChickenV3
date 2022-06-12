aef32eb5466cade3c87f6b9206c344529053427e
PirisAran <apiris@edu.tecnocampus.cat>
2022-06-12 11:18:34 +0200

    hola

--- Assets/Autodestroy.cs
+++ Assets/Autodestroy.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Autodestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float LifeTime = 2;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Destroy(gameObject,LifeTime);
     }
 }
