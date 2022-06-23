a8de082b9f2ff6487bbc1e96480c50e6c396b655
PirisAran <apiris@edu.tecnocampus.cat>
2022-06-23 22:58:42 +0200

    cinematica acabada

---
+++ Assets/endVideo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endVideo : MonoBehaviour
+{
+    public GameObject videoPlayer;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("StopVideo", 58.0f);
+        if (Input.GetKeyDown("space"))
+        {
+            StopVideo();
+        }
+    }
+
+    private void StopVideo()
+    {
+        videoPlayer.SetActive(false);
+        Destroy(videoPlayer);
+    }
+}
